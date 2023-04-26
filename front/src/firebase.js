9282a1a5b89f746483434741950c59f75e60a784
Ronaldo Reis <59805760+douglucio@users.noreply.github.com>
2023-04-26 08:24:13 -0300

    Editando firebase config
    Editando firebase config

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzm_7MCAnGqE7kQiENOHbZ2vnakRjGVww",
-    authDomain: "as-dev-web.firebaseapp.com",
-    projectId: "as-dev-web",
-    storageBucket: "as-dev-web.appspot.com",
-    messagingSenderId: "959735811603",
-    appId: "1:959735811603:web:f5b153504f3d2d2a1ff9e6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   if (!firebase.apps.length) {
